66c4de9fe091bdb4a8a69aa487a23b58f931dc08
hyunmin0317 <choihm9903@naver.com>
2023-10-18 22:10:19 +0900

    feat: e-campus 인증 메서드 작성 (#1)

---
+++ sangmyung_univ_auth/authenticate.py
@@ -0,0 +1,23 @@
+import requests
+from requests import Session
+from bs4 import BeautifulSoup as bs
+
+
+def session_request(username: str, password: str):
+    with requests.session() as s:
+        user_info = {"username": username, "password": password}
+        request = s.post("https://ecampus.smu.ac.kr/login/index.php", data=user_info)
+        if request.url == "https://ecampus.smu.ac.kr/":
+            return s
+        return
+
+
+def get_userinfo(session: Session) -> dict:
+    request = session.get("https://ecampus.smu.ac.kr/user/user_edit.php")
+    source = request.text
+    soup = bs(source, "html.parser")
+    return {
+        "name": soup.find('input', id='id_firstname').get('value'),
+        "department": soup.find('input', id='id_department').get('value'),
+        "email": soup.find('input', id='id_email').get('value')
+    }
