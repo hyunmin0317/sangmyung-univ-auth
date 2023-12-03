998e0dc390cb4e26f3d45b9399edd1fca3766bcc
hyunmin0317 <choihm9903@naver.com>
2023-12-04 00:32:48 +0900

    feat: 샘물 포탈 sso token 인증 방식 (#15)

---
+++ sangmyung_univ_auth/authenticator/portal_ssotoken.py
@@ -0,0 +1,23 @@
+import requests
+
+
+def authenticate(username: str, password: str):
+    with requests.session() as s:
+        url = 'https://smsso.smu.ac.kr/Login.do'
+        user_info = {'user_id': username, 'user_password': password}
+        request = s.post(url, data=user_info)
+        s.get('https://smul.smu.ac.kr/index.do')
+        if request.url == url:
+            return
+        return s, username
+
+
+def get_userinfo(session) -> dict:
+    session, username = session
+    response = session.post('https://smul.smu.ac.kr/UsrSchMng/selectStdInfo.do', data={'@d#': '@d1#', '@d1#tp': 'dm', '_AUTH_MENU_KEY': 'usrCPsnlInfoUpd-STD', '@d1#strStdNo': username})
+    data = response.json()['dsStdInfoList'][0]
+    return {
+        'name': data['NM_KOR'],
+        'department': data['TMP_DEPT_MJR_NM'].split()[-1],
+        'email': data['EMAIL']
+    }
